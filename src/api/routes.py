83e2acedc82ab026d6636376fd38da55309ea234
ChiaraR3 <chiara.r.3@live.it>
2021-10-06 22:15:02 +0000

    ok

--- src/api/routes.py
+++ src/api/routes.py
@@ -46,7 +46,7 @@ def get_users():
     
     return jsonify(users), 200
 
-@app.route("/login", methods=["POST"])
+@api.route("/login", methods=["POST"])
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
@@ -54,17 +54,18 @@ def create_token():
     user = User.query.filter_by(email=email, password=password).first()
     if user is None:
         # the user was not found on the database
-        return jsonify({"msg": "Bad username or password"}), 401
+        return jsonify({"msg": "Bad email or password"}), 401
     
     # create a new token with the user id inside
     access_token = create_access_token(identity=user.id)
     return jsonify({ "token": access_token, "user_id": user.id })
 
-@app.route("/protected", methods=["GET"])
+
+@api.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
     # Access the identity of the current user with get_jwt_identity
     current_user_id = get_jwt_identity()
-    user = User.filter.get(current_user_id)
+    user = User.query.get(current_user_id)
     
     return jsonify({"id": user.id, "email": user.email }), 200
--- src/front/js/pages/protected.js
+++ src/front/js/pages/protected.js
@@ -9,23 +9,23 @@ export const Protected = () => {
 	const { store, actions } = useContext(Context);
 	const [data, setData] = useState(" ");
 
-	const protectedData = async (email, password) => {
+	const protectedData = async () => {
 		// retrieve token form localStorage
 		const token = localStorage.getItem("jwt-token");
-		const response = await fetch(`https://3001-harlequin-lungfish-0yoeppnf.ws-eu18.gitpod.io/api/protected`, {
+		const response = await fetch(process.env.BACKEND_URL + "/api/protected", {
 			method: "GET",
 			headers: {
 				"Content-Type": "application/json",
 				Authorization: "Bearer " + token
 			}
 		});
-		if (!resp.ok) throw Error("There was a problem in the login request");
+		if (!response.ok) throw Error("There was a problem in the login request");
 		const responseJson = await response.json();
 		setData(responseJson);
 	};
 
 	useEffect(() => {
-		if (store.usertoken === null) history.push("/login");
+		if (store.user_token === null) history.push("/login");
 		else protectedData();
 	}, []);
 
@@ -36,7 +36,7 @@ export const Protected = () => {
 				<img src={rigoImageUrl} />
 			</p>
 			<div className="alert alert-info" />
-			<h1>YOUR PROTECTED DATA HERE</h1>
+			<h1>HI! YOUR PROTECTED DATA HERE</h1>
 		</div>
 	);
 };
